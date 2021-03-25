cbf10c4fc6704271e0f524f4bd25d838053c9846
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-03-26 00:20:11 +0600

    first try to do CRUD operation with database

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const password = '.qmZGJ.A5-Fvfts';
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const uri = "mongodb+srv://yourNaimur:.qmZGJ.A5-Fvfts@cluster0.avi8n.mongodb.net/organicDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+client.connect(err => {
+    const collection = client.db("organicDB").collection("products");
+
+    app.get('/products', (req, res) => {
+        //collection.find({}).limit(4)
+        collection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        collection.insertOne(product)
+            .then(res => console.log('One product added'))
+        res.redirect('/');
+    })
+
+    //client.close();
+
+    app.delete('/delete/:id', (req, res) => {
+        collection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then((result) => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+    app.get('/product/:id', (req, res) => {
+        collection.find({ _id: ObjectID(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+    app.patch('/update/:id', (req, res) => {
+        collection.updateOne({ _id: ObjectID(req.params.id) }, {
+            $set: { price: req.body.price, quantity: req.body.quantity }
+        })
+            .then(result => {
+                res.send(result.modifiedCount > 0);
+            })
+    })
+});
+
+
+app.listen(3000);
